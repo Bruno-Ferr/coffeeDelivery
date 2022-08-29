ecf0bb71fa64cc809afe5e3579a3fec2c91570e7
Bruno-Ferr <fbruno233@gmail.com>
2022-08-28 23:42:07 -0300

    Navigation to checkout and home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,19 +2,22 @@ import { MapPin, ShoppingCart } from "phosphor-react";
 import './styles.scss'
 
 import logo  from '../../assets/Logo.png'
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <header>
-      <img src={logo} alt="" />
+      <Link to="/">
+        <img src={logo} alt="" />
+      </Link>
       <div className="navbar">
         <div className="location">
           <MapPin size={22} weight="fill" />
           <p>São Paulo, SP</p>
         </div>
-        <button className="cart">
+        <Link to="/checkout"  className="cart">
           <ShoppingCart size={22}  weight="fill" />
-        </button>
+        </Link>
       </div>
     </header>
   )
